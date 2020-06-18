5326080eb0660647ec642e0d1a01cac157d2cb5a
BylaneA <billieabdiw@gmail.com>
2020-06-18 14:51:40 +0200

    1ere boucle pour la table de x

---
+++ exo1.php
@@ -0,0 +1,16 @@
+<?php 
+    include("common/header.php");
+    include("common/menu.php");
+?>
+
+<h1>Affichage des tables de multiplication</h1>
+
+<?php 
+    for ($i = 1; $i <= 10; $i++){
+        echo $i;
+    }
+?>
+
+<?php 
+    include("common/footer.php");
+?>
