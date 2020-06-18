dd2c39378dd2b39c6c1db1f340272bf1ed6a9365
BylaneA <billieabdiw@gmail.com>
2020-06-18 15:20:32 +0200

    definir une constante

--- exo1.php
+++ exo1.php
@@ -6,8 +6,11 @@
 <h1>Affichage des tables de multiplication</h1>
 
 <?php 
+    //définition d'une constante
+    define(NUMERO, 4);
+
     for ($i = 1; $i <= 10; $i++){
-        echo $i . "x" . 3 . "=" . $i * 3 . "<br/>";
+        echo $i . "x" . NUMERO . "=" . $i * NUMERO . "<br/>";
     }
 ?>
 
