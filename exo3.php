59e352557eec3c2c26896586a68f10b53db5c85b
BlueberrySeven <bilaneabdidjama@gmail.com>
2021-02-19 16:42:21 +0100

    MAJ exo 3

--- exo3.php
+++ exo3.php
@@ -5,26 +5,34 @@
 
 <h1>Cercle - Périmètre et Aire</h1>
 <form action="#" method="POST">
-<label for="rayon">Rayon d'un cercle</label><input type="number" name="rayon">
-<label for="perimetre">Périmètre</label><input type="checkbox" id="perimetre" name="perimetre">
-<label for="aire">Aire</label><input type="checkbox" id="aire" name="aire">
+<label for="rayon">Rayon d'un cercle</label>
+<input type="number" name="rayon" id="rayon"><br/>
+<label for="perimetre">Périmètre</label>
+<input type="checkbox" id="perimetre" name="perimetre" checked><br/>
+<label for="aire">Aire</label>
+<input type="checkbox" id="aire" name="aire" checked><br/>
 <input type="submit" value="Envoyer">
 
 </form>
 
 <?php 
     
-    if (isset($_POST["hauteur"])){
-        echo "La hauteur est de " . $_POST["hauteur"] . "<br/>";
-        $motif= "";
-        for( $i=0; $i <= $_POST["hauteur"]; $i++){
-            $motif.= "xx";
-            echo $motif . "<br/>" ;
+    // if (isset($_POST["rayon"])){
+    //     echo "Le périmètre est de " . $_POST["rayon"]*2*pi(). "<br/>";
+    //     echo "L'aire est de " . pi()*$_POST["rayon"]*$_POST["rayon"]. "<br/>";
+    // }
+
+    if(isset($_POST["rayon"]) && $_POST["rayon"] >0){
+        $rayon = $_POST["rayon"];
+
+        if(isset($_perimetre)){
+            echo "Le périmètre est de " . $_POST["rayon"]*2*pi(). "cm" ."<br/>";
         }
-        for( $i=0; $i < $_POST["hauteur"]-1; $i++){
-            $motif = substr($motif, 0, strlen($motif)-2);
-            echo $motif . "<br/>" ;
+        if(isset($_aire)){
+            echo "L'aire est de " . pi()*$_POST["rayon"]*$_POST["rayon"]. "m2" . "<br/>";
         }
+    } else {
+        echo "Saisir une valeur";
     }
     
 ?>
