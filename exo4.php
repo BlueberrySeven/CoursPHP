e010ed77a96aa696ad93cb92323eeca796410c95
BlueberrySeven <bilaneabdidjama@gmail.com>
2021-03-02 13:15:25 +0100

    refactoring exo 4

--- exo4.php
+++ exo4.php
@@ -2,19 +2,30 @@
     include("common/header.php");
     include("common/menu.php");
 
-    $ChAlea = 7;
+    session_start();
+    if(!isset($_SESSION['chAlea'])){
+        $_SESSION['chAlea'] = rand(1,10);
+    }
 ?>
 
 <h1>Chiffre aléatoire</h1>
 <form action="#" method="POST">
-<label for="chiffre">Chiffre choisi</label>
-<input type="number" name="chiffre" id="chiffre">
-<input type="submit" value="Envoyer">
+    <input type="hidden" name="reinit" value="yes">
+    <input type="submit" value="Réinitialiser">
+</form>
+<form action="#" method="POST">
+    <label for="chiffre">Chiffre choisi</label>
+    <input type="number" name="chiffre" id="chiffre">
+    <input type="submit" value="Envoyer">
 
 </form>
 
 <?php 
-    
+    if(isset($_POST['reinit']) && $_POST['reinit'] === "yes"){
+        $_SESSION['chAlea'] = rand(1,10);
+    }
+    $ChAlea = $_SESSION['chAlea'];
+
     if(isset($_POST["chiffre"]) && $_POST["chiffre"] > 0){
         $ChSaisi = (int)$_POST["chiffre"];
         if($ChSaisi === $ChAlea){
