ea5ff4fbd26215cffc84642e21cbd64f2a369978
BlueberrySeven <bilaneabdidjama@gmail.com>
2021-03-03 16:34:20 +0100

    MAJ exo 5 : test 1 : non

--- exo5.php
+++ exo5.php
@@ -1,21 +1,36 @@
 <?php 
     include("common/header.php");
     include("common/menu.php");
-    // session?
-    // form pour saisir le nombre de notes
-    // si par exemple 4 nb, affichage de 4 ligne dans lesquels on notera les nombres
-    // calcul de la moyenne de ces notes
+    //Réaliser un tableau contenant les valeurs : 2,6,12,5,26,34,40,60
+    //Réaliser une fonction qui vérifie si un tableau ne contient que des valeurs paires ou non
+    //Afficher un message pour l'indiquer à l'utilisateur
+    //Le tableau contient des valeurs impaires
 ?>
 
-<h1>Calculer une moyenne</h1>
-<form action="#" method="POST">
-    <label for="moyenne">Nombre de notes</label>
-    <input type="number" name="nombre" id="nombre">
-    <input type="submit" value="Ajouter">
-</form>
+<h1>Résultat</h1>
+
 
 <?php 
+   //array
+   $tbNb = [2,6,12,5,26,34,40,60];
 
+   // fonction verification array
+   function verifTab($tab){
+       for ($i=0; $i < count($tab)-1; $i++) { 
+           if ($tab[0]%2 == 1) {
+               return true;
+           }
+       }
+       return false;
+   }
+
+   //passer tnNb en argument
+   if (verifTab($tbNb)) {
+       echo "Ce tableau ne contient que des chiffres pairs";
+   } else {
+       echo "Ce tableau contient des chiffres impairs";
+   }
+   
 ?>
 
 <?php 
